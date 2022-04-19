38ffafd056fe96dfcb6b381b5958ccf5c1853e72
Ornela Anahi Lugo Aquino <nelladelugo2@gmail.com>
2022-04-19 18:39:44 -0300

    APIRESTful

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 8085
+const PORT = process.env.PORT || 8080
 
 const routerProductos = require ('./routes/productos')
 
